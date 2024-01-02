c8325a02078fc885f7b0e406ce830375a6c0688d
unknown <javedshahzadnns@gmail.com>
2024-01-02 11:50:44 +0500

    NEW CHANGES

--- src/app/Pages/upcoming-events/upcoming-events.page.ts
+++ src/app/Pages/upcoming-events/upcoming-events.page.ts
@@ -45,21 +45,26 @@ export class UpcomingEventsPage implements OnInit {
       const eventRegistDB = await this.angularFireStore.collection('eventRegisteredAttendees',res => {
         return res.orderBy('createdAt','desc').where("userId","==",user.userId).where("eventId","==",eventData.eventId)})
       const eventRegData = await eventRegistDB.valueChanges()
-      eventRegData.subscribe((response:any) =>{
+       let eventsubs= eventRegData.subscribe((response:any) =>{
+
+        console.log(response)
        if(response.length > 0){
         this.restSr.hideLoader();
+        eventsubs.unsubscribe();
         this.restSr.toast("You already registered with this event!");
        }else{
         let today = new Date();
         eventData.createdAt = today;
         eventData.userId=user.userId;
         eventData.user=user;
+        eventsubs.unsubscribe();
         this.angularFireStore.collection("eventRegisteredAttendees").add(eventData).then((respone:any)=>{
           this.angularFireStore.collection('eventRegisteredAttendees').doc(respone.id).update({
             'RegisteredAttendeesId': respone.id
         });
+        this.restSr.toast("You are registered with this event!");
         this.restSr.hideLoader();
-        
+       
         })
        }
         
